3f0ee0637326f626d4467d46bbf7551c2b0a600a
Thomas-byte007 <2057335724.@qq.com>
2025-11-17 20:04:40 +0800

    Initial commit

---
+++ Web-project-01/springboot-web-startquickly/src/test/java/com/itheima/springbootwebstartquickly/SpringbootWebStartquicklyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springbootwebstartquickly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebStartquicklyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
